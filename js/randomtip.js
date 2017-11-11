d75cb2bf730e3043f2a6761f4ba5bfb957b34219
Nett18 <kressckerl@gmail.com>
2017-11-11 21:42:44 +0100

    Account

--- js/randomtip.js
+++ js/randomtip.js
@@ -1,9 +1,10 @@
 //No sneak peaks
 var loadingtips = [
     'Did you know <b>Frostburn-Games</b> spelled backwards is <b>semaG-nrubtsorF</b>.',
-    'Did you know this is the second loading tip?.',
-    'Did you know this loading tip is silly?.',
-    'Did you know why the chicken walked over the road?'
+    'Did you know this is the second loading tip?',
+    'Did you know this loading tip is silly?',
+    'Did you know why the chicken walked over the road?',
+    'When I was little I thought <i>RPG</i> meant the game included an RPG'
 ]
 var randomIndex = Math.floor(Math.random() * loadingtips.length);
 var randomElement = loadingtips[randomIndex];
